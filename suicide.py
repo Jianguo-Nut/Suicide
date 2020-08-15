693c9ae308c44b06a8f73eba2138fdf4c4f71548
Jianguo_Nut <68621503+Jianguo-Nut@users.noreply.github.com>
2020-08-15 11:55:49 +0800

    Add files via upload

---
+++ suicide.py
@@ -0,0 +1,10 @@
+#-*-coding:utf8;-*-
+
+def on_user_info(server,info):
+    if info.content == '!!kill' and info.is_player:
+        server.execute('kill' + info.player)
+
+
+
+def on_load(server,old):
+    server.add_help_message('!!kill' , '自杀')
